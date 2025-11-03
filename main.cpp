c0746477706ede974ce9dda632bd995d7ebb09ca
thembones79 <michal.szumnarski@gmail.com>
2025-11-03 11:23:03 +0100

    jaja ding dong

--- main.cpp
+++ main.cpp
@@ -7,14 +7,37 @@ int main() {
 
   InitWindow(windowDimensions[0], windowDimensions[1], "Classy Clash");
   Texture2D background = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
+  Vector2 mapPos{0.0, 0.0};
 
   SetTargetFPS(60);
   while (!WindowShouldClose()) {
     BeginDrawing();
 
     ClearBackground(WHITE);
-    Vector2 bg1Pos{0.0, 0.0};
-    DrawTextureEx(background, bg1Pos, 0.0, 4.0, WHITE);
+
+
+
+Vector2 direction{};
+        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+        if (IsKeyDown(KEY_D)) direction.x += 1.0;
+        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+        if (IsKeyDown(KEY_S)) direction.y += 1.0;
+
+        if (Vector2Length(direction) != 0.0){
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    DrawTextureEx(background, mapPos, 0.0, 4.0, WHITE);
 
     EndDrawing();
   }
